d35b768b05c6393a8448d2cf583d14c42b9b7397
keychera <kyogatama@gmail.com>
2023-07-03 16:54:55 +0700

    level experiment

--- assets/level.cljs
+++ assets/level.cljs
@@ -1,8 +1,24 @@
 (ns level)
 
-(defn exp_up []
+(defonce level (atom 0))
+
+(defn set-level [lvl]
+  (let [level-count (. js/document (getElementById "level-count"))]
+    (set! (. level-count -innerHTML) (str "Lv. " lvl))))
+
+(defn level-up []
+  (set-level (swap! level inc)))
+
+(set-level @level)
+
+(defn exp_up [exp-val]
   (let [exp-bar (. js/document (getElementById "exp-bar"))
-        exp-val (. exp-bar -value)]
-    (set! (. exp-bar -value) (+ exp-val 5))))
+        max-exp (. exp-bar -max)
+        current-exp (. exp-bar -value)
+        next-exp (+ current-exp exp-val)]
+    (if (>= next-exp max-exp)
+      (do (set! (. exp-bar -value) (- next-exp max-exp))
+          (level-up))
+      (set! (. exp-bar -value) next-exp))))
 
 (set! (.-exp_up js/window) exp_up)
